9f0ef939e77d7bfd0f67bca98d3b782e4bd10271
abedaproza <146949619+abedaproza@users.noreply.github.com>
2024-10-07 22:22:57 +0800

    Add files via upload

---
+++ quiz_one.py
@@ -0,0 +1,21 @@
+#create variables that will store the names of your groupmates
+Name_Member1 = "Jonadel N. Cabang"
+Name_Member2 = "Abegail A. Daproza"
+Name_Member3 = "Francene Gian Pais"
+#create variables that contains their age in whole number
+Age_Member1 = str(22)
+Age_Member2 = str(23)
+Age_Member3 =str(20)
+#create variables that contains their weekly allowance in decimal form
+All_Member1 = float(2000)
+All_Member2 = float(1000)
+All_Member3 = float(2000)
+#print all results formatted as follows: use concat
+print ("Team Name:")
+print("Member1:",(Name_Member1),", her age is", (Age_Member1), ", allowance per week is", (All_Member1)) 
+print("Member2:",(Name_Member2),", her age is", (Age_Member2), ", allowance per week is", (All_Member2)) 
+print("Member3:", (Name_Member3),", her age is", (Age_Member3), ", allowance per week is", (All_Member3)) 
+#create variables to store the length of the names of the members and print them formatted as follows:
+print("Member 1 consist of", len(Name_Member1), "characters")
+print("Member 2 consist of", len(Name_Member2), "characters")
+print("Member 3 consist of", len(Name_Member3), "characters")
---
+++ quiz_two.py
@@ -0,0 +1,34 @@
+Name_Member1 = "Jonadel N. Cabang"
+Name_Member2 = "Abegail A. Daproza"
+Name_Member3 = "Francene Gian Pais"
+
+#perform and print the following math operations for all the age and allowances:
+Age_Member1 = 22
+Age_Member2 = 23
+Age_Member3 = 20
+
+All_Member1 = float(2000)
+All_Member2 = float(1000)
+All_Member3 = float(2000)
+
+name1=len(Name_Member1)
+name2=len(Name_Member2)
+name3=len(Name_Member3)
+
+Age_add = (int (Age_Member2) + (Age_Member2) + (Age_Member3))
+print(Age_add) 
+
+Age_sub = (int (Age_Member2) - (Age_Member2) - (Age_Member3))
+print(Age_sub)
+
+AgeAll_prod1 = (int(Age_Member1) * (All_Member1))
+print(AgeAll_prod1)
+AgeAll_prod2 = (int(Age_Member2) * (All_Member2))
+print(AgeAll_prod2)
+AgeAll_prod3 = (int(Age_Member3) * (All_Member3))
+print(AgeAll_prod3)
+
+#perform and print. compare all the result
+print(Age_Member1 == Age_Member2, Age_Member2 == Age_Member3, Age_Member1 == Age_Member3)
+print(name1 == name2, name2 == name3 , name1 == name3)
+
